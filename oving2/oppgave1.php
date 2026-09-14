307d757ad048035ecb955f65fe70b3d677f1b415
Larsthei <lars.theimann@live.no>
2026-09-14 14:21:19 +0200

    Legger til oving 2

---
+++ oving2/oppgave1.php
@@ -0,0 +1,14 @@
+<?php
+
+$svar = $_POST["svar"];
+
+if ($svar == "9") 
+  {
+  print ("Riktig. 3 ganger 3 er 9.");
+} 
+else 
+  {
+  print ("Feil. 3 ganger 3 er ikke $svar. 3 ganger 3 er 9.");
+}
+
+?>
