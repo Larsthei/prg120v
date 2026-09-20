d65ba1037d612c8d94d4ab0667a72f48db01857d
Larsthei <lars.theimann@live.no>
2026-09-20 19:41:19 +0200

    liten fiks o8

--- oving3/oppgave8.php
+++ oving3/oppgave8.php
@@ -4,7 +4,7 @@ $tall1 = $_POST['tall1'];
 $tall2 = $_POST['tall2'];
 $tall3 = $_POST['tall3'];
 $tall4 = $_POST['tall4'];
-$tall5 = $_POST['tall5'];'
+$tall5 = $_POST['tall5'];
 
 $tall = array($tall1, $tall2, $tall3, $tall4, $tall5);
 
