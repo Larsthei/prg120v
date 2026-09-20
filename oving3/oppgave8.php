5800319700e9acfd98df8af3d61c2622de3d6cfd
Larsthei <lars.theimann@live.no>
2026-09-20 19:39:49 +0200

    legger inn o3o8

---
+++ oving3/oppgave8.php
@@ -0,0 +1,16 @@
+<?php
+
+$tall1 = $_POST['tall1'];
+$tall2 = $_POST['tall2'];
+$tall3 = $_POST['tall3'];
+$tall4 = $_POST['tall4'];
+$tall5 = $_POST['tall5'];'
+
+$tall = array($tall1, $tall2, $tall3, $tall4, $tall5);
+
+for ($i = 0; $i <= 4; $i++)
+{
+print("$tall[$i] ");
+}
+
+?>
