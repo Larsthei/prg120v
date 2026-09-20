c7cf60bd0f337a8f3efdb565d9aa16adea63d9fc
Larsthei <lars.theimann@live.no>
2026-09-20 12:41:36 +0200

    Oving1 Oppgave 2 legges inn

---
+++ oving1/oppgave2.php
@@ -0,0 +1,12 @@
+<?php
+
+$tall1=$_POST ["tall1"];
+$tall2=$_POST ["tall2"];
+
+$summen=$tall1+$tall2;
+$differansen=$tall1-$tall2;
+
+print ("Tall 1 er $tall1 <br />");
+print ("Tall 2 er $tall2 <br />");
+print ("Summen er $summen <br />");
+print ("Differansen er $differansen <br />");
